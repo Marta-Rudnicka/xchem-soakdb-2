fabdb27c3d05c4725b5e5661414ad3bc6ac2640e
Marta Rudnicka <marta.rudnicka@diamond.ac.uk>
2020-10-19 14:20:05 +0100

    Replaced hard-coded html tables with python function

--- compound_screen/views.py
+++ compound_screen/views.py
@@ -1,43 +1,58 @@
 from django.shortcuts import render
 
+def generate_table_body(html_string):
+	rows = ""
+	for i in range(61):
+		rows = rows + "<tr><td>" + str(i+1) + "</td>" + html_string + "</tr>\n"
+	
+	return rows
+
+
 def source(request):
 	return render(request, "compound_screen/source.html", {
 		"title": "Source compounds table",
+		"tbody": generate_table_body('<td class="lib">[data]</td><td class="plate">[data]</td><td class="cc">[data]</td><td class="code">[data]</td><td class="smiles">[data]</td><td class="c2d"><img src="../static/compound_screen/pic.png" alt="alttext"></td><td class="p1">[data]</td><td class="p2">[data]</td><td class="p3">[data]</td><td class="p4">[data]</td>'),
 		"table_caption": "Source compounds", 
 	})
 
 def crystals(request):
 	return render(request, "compound_screen/crystals.html", {
 	"title": "Crystals table",
+	"tbody": generate_table_body('<td class="plate">[data]</td><td class="drop">[data]</td><td class="x">[data]</td><td class="y">[data]</td><td class="score">[data]</td><td class="crystal-field"><input type="checkbox" class="show-image"><label>show image</label><br><img class="crystal-pic" src="../static/compound_screen/crystal.png" alt="alttext"><br></td><td><button class="remove-button">Remove</button><button class="include-button">Include</button>'),
 	"table_caption": "Crystals", 
 	})
 
 def batches(request):
 	return render(request, "compound_screen/batches.html", {
 	"title": "Batches table",
+	"tbody": generate_table_body('<td>[data]</td><td>[data]</td><td>[data]</td><td>[data]</td><td><input class="bno" type="number" min="1"><td>[data]</td>'),
 	"table_caption": "Batches", 
 	})
 
 def soak(request):
 	return render(request, "compound_screen/soak.html", {
 	"title": "Soak table",
+	"tbody": generate_table_body('<td>[data]</td><td class="cs"></td><td>[data]</td><td class="tv"></td><td>[data]</td><td>[data]</td>'),
 	"table_caption": "Soaking", 
 	})
 
 def cryo(request):
 	return render(request, "compound_screen/cryo.html", {
 	"title": "Cryo table",
+	"tbody": generate_table_body('<td>[data]</td><td>[data]</td><td>[data]</td><td>[data]</td><td>[data]</td>'),
 	"table_caption": "Cryo", 
 	})
 
 def harvesting(request):
 	return render(request, "compound_screen/harvesting.html", {
 	"title": "Harvesting table",
+	"tbody": generate_table_body(''),
 	"table_caption": "Harvesting", 
 	})
 
 def collection(request):
 	return render(request, "compound_screen/collection.html", {
 	"title": "Data collection",
+	"tbody": generate_table_body(''),
 	"table_caption": "Data collection", 
 	})
--- experiment_preparation/views.py
+++ experiment_preparation/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+
+def generate_table_body(html_string):
+	rows = ""
+	for i in range(61):
+		rows = rows + "<tr><td>" + str(i+1) + "</td>" + html_string + "</tr>\n"
+	
+	return rows
+
 
 # Create your views here.
 def index(request):
@@ -9,6 +16,7 @@ def cherrypicking(request):
 	return render(request, "experiment_preparation/c-selection.html", {
 		"title": "Compound Selection",
 		"page_heading": "XChem in-house library explorer", 
+		"tbody": generate_table_body('<td class="lib">[data]</td><td class="plate">[data]</td><td class="code">[data]</td><td class="smiles">[data]</td><td class="c2d"><img alt="alttext" src="../static/experiment_preparation/pic.png"></td><td class="p1">[data]</td><td class="p2">[data]</td><td class="p3">[data]</td><td class="p4">[data]</td><td class="priority">(none)</td><td><input class="select" type="checkbox"></td>'),
 		"page_info": "Select the most suitable compounds from XChem in-house libraries, and save your selection for your future experiment. You can use various filters and sorting options to refine your selection.", 
 		"table_caption": "Compounds in XChem in-house libraries", 
 	})
@@ -16,7 +24,8 @@ def cherrypicking(request):
 def c_check(request):
 	return render(request, "experiment_preparation/c-check.html", {
 		"title": "Selected Compounds",
-		"page_heading": "Compound selected for the current experiment", 
+		"page_heading": "Compound selected for the current experiment",
+		"tbody": generate_table_body('<td class="lib">[data]</td><td class="plate">[data]</td><td class="code">[data]</td><td class="smiles">[data]</td><td class="c2d"><img alt="alttext" src="../static/experiment_preparation/pic.png"></td><td class="p1">[data]</td><td class="p2">[data]</td><td class="p3">[data]</td><td class="p4">[data]</td><td class="priority">High</td><td><button class="remove-button">Remove</button><button class="include-button">Include</button></td>'),
 		"page_info": "Make a list of compounds to be used in the the current experiment only", 
 		"table_caption": "Compounds selected for the experiment: currently available", 
 	})
