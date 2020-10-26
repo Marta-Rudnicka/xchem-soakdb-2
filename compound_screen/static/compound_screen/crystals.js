6ef6130a7db35eb6fd46b1f0bca9822ca83be3fb
Marta Rudnicka <marta_rudnicka@vp.pl>
2020-10-26 14:41:46 +0000

    crystal gallery: added well numbers and used crystals

--- compound_screen/static/compound_screen/crystals.js
+++ compound_screen/static/compound_screen/crystals.js
@@ -1,96 +1,5 @@
-//small functions to make setTimeout easier	
-function hideParent(element){
-	element.parentElement.className = 'hidden';
-}
-	
-function show(element){
-	element.style.display = 'block';
-}
-	
-function showDiv(element){
-	element.className = 'gallery';
-}
-	
-function showHidden(element){
-	element.hidden = false;
-}
-	
-function clearStyle(element) {
-	element.style="";
-}
-
-function updateTotals() {
-	var totalAccepted = 0;
-	var totalRejected = 0;
-	
-	document.querySelectorAll('.counter-accepted').forEach(span => {
-		totalAccepted = totalAccepted + parseInt(span.innerHTML);
-		})
-	document.querySelectorAll('.counter-rejected').forEach(span => {
-		totalRejected = totalRejected + parseInt(span.innerHTML);
-		})
-		
-	document.getElementById('total-accepted').innerHTML = totalAccepted;
-	document.getElementById('total-rejected').innerHTML = totalRejected;
-	
-}
-
-function hideTile(image) {
-		
-		const parent = image.parentElement;
-		
-		//animate hiding a tile
-		parent.querySelector('.bin-pic').hidden = true;
-		parent.querySelector('.info-pic').hidden = true;
-		parent.style = 'animation-play-state: running; animation-duration: 0.4s';
-		parent.querySelector('.main-pic').style = 'animation-play-state: running; animation-duration: 0.4s';
-		setTimeout(hideParent, 400, image);
-		setTimeout(clearStyle, 400, parent);
-		setTimeout(clearStyle, 400, parent.querySelector('.main-pic'));
-}
-
-function showTile(binParent, containerClass) {
-		
-		//update counters of accepted and rejected crystals in each plate
-		const counterAccepted = binParent.parentElement.parentElement.querySelector(containerClass).parentElement.querySelector('.counter-accepted');
-		const counterRejected = binParent.parentElement.parentElement.querySelector(containerClass).parentElement.querySelector('.counter-rejected');
-		var accepted = parseInt(counterAccepted.innerHTML);
-		var rejected = parseInt(counterRejected.innerHTML);		
-		if (containerClass === '.rejected') {
-			counterAccepted.innerHTML = accepted - 1;
-			counterRejected.innerHTML = rejected + 1;
-		}
-		else {
-			counterAccepted.innerHTML = accepted + 1;
-			counterRejected.innerHTML = rejected - 1;
-		}
-		updateTotals();
-		
-		//find a hidden div to show	
-		const galleryDivs = binParent.parentElement.parentElement.querySelector(containerClass).querySelectorAll('div');
-		var newDiv = null;
-		var found = false;
-		galleryDivs.forEach( div => {
-			if (found === true) {
-				return;
-			}
-			if (div.className === 'hidden') {
-				newDiv = div;
-				found = true;
-				console.log('showDiv: ', showDiv);
-			}
-		})
-		
-		//animate showing a new tile
-		showDiv(newDiv);
-		newDiv.style = 'animation-play-state: running; animation-direction: reverse; animation-duration: 0.4s';
-		newDiv.querySelector('.main-pic').style = 'animation-play-state: running; animation-direction: reverse; animation-duration: 0.4s';
-		setTimeout(clearStyle, 400, newDiv);
-		setTimeout(clearStyle, 400, newDiv.querySelector('.main-pic'));
-		setTimeout(showHidden, 420, newDiv.querySelector('.bin-pic'));
-		setTimeout(showHidden, 420, newDiv.querySelector('.info-pic'));
-		showDiv.className = "gallery";
-}
+/* Defines actions of buttons and icons in the /compound_screen/crystals
+ * path. Uses functions defined in crystals_functions.js	*/
 
 document.addEventListener('DOMContentLoaded', () => {
 	updateTotals();	
@@ -98,50 +7,106 @@ document.addEventListener('DOMContentLoaded', () => {
 	//rejecting or recycling a crystal	
 	document.querySelectorAll('.bin-pic').forEach(image => {
 		image.onclick = () => {
-			const parent = image.parentElement
+			const parentTile = image.parentElement.parentElement;
 			
-			//make crystal info box empty if you remove highlighted crystal
-			if (parent.className == 'gallery-highlighted'){
-				document.getElementById('infobox').style.visibility = "hidden";
-				document.getElementById('empty-crystal-info').style.visibility = "visible";
-				document.getElementById('crystal-info').style.border = "solid black 3px";				
-			}
-			
-			//hide-element
-			 hideTile(image);
+			hideTile(image);
 			
 			//show new element in the other div
-			if (parent.parentElement.className === 'accepted') {
-				showTile(parent, '.rejected');			
+			if (parentTile.parentElement.className === 'accepted') {
+				showTile(parentTile, '.rejected');
+				updateCounter(parentTile, '.rejected');			
 			}
 			else {
-				showTile(parent, '.accepted');		
+				showTile(parentTile, '.accepted');
+				updateCounter(parentTile, '.accepted');
 			}
-		}
-		
-		//pretend to display info about a crystal
-		document.querySelectorAll('.info-pic').forEach(image => {
-		image.onclick = () => {
-			document.getElementById('infobox').style.visibility = "visible";
-			document.getElementById('empty-crystal-info').style.visibility = "hidden";
-			document.getElementById('crystal-info').style.border = "solid #66ff00 6px";
-			document.querySelectorAll('.gallery-highlighted').forEach(div => {
-				div.className = "gallery"
-			})
-			image.parentElement.className = "gallery-highlighted"
 			
-			}
-		})
+		}
 	})
-	
+		
+	//display info about a crystal
+	document.querySelectorAll('.show-pic').forEach(image => {
+		image.onclick = () => {
+			showInfoBox(image);
+		}
+	})
+
+	//hide info about crystal
+	document.querySelectorAll('.hide-pic').forEach(image => {
+		image.onclick = () => {
+			hideInfoBox(image);
+		}
+	})
+			
+	//show new plate
 	document.getElementById('texrank').onclick = () => {
 		document.querySelectorAll('section').forEach(section => {
 			section.style.display = 'block';
 		})
 		document.getElementById('summary').hidden = false;
-		document.getElementById('crystal-info').hidden = false;
-		document.getElementById('texrank').hidden = true;
 		document.getElementById('crystal-form').style.display = 'grid';
 	}
 	
+	//show all crystal info in a plate
+	document.querySelectorAll('.show-all').forEach(button => {
+		const parentPlate = button.parentElement.parentElement;
+		button.onclick = () => {
+			parentPlate.querySelectorAll('.show-pic').forEach(image => {
+				showInfoBox(image)
+			})
+			parentPlate.querySelector('.hide-all').hidden = false;
+			button.hidden = true;
+		}
+	})
+	//hide all crystal info in a plate
+	document.querySelectorAll('.hide-all').forEach(button => {
+		const parentPlate = button.parentElement.parentElement;
+		button.onclick = () => {
+			parentPlate.querySelectorAll('.hide-pic').forEach(image => {
+				hideInfoBox(image);
+			})
+			parentPlate.querySelector('.show-all').hidden = false;
+			button.hidden = true;
+		}
+	})
+	
+	document.querySelectorAll('.show-plate').forEach(button => {
+		const parentPlate = button.parentElement.parentElement
+		button.onclick = () => {
+			parentPlate.querySelector('.plate-body').hidden = false;
+			parentPlate.querySelector('.show-plate').hidden = true;
+			parentPlate.querySelector('.hide-plate').hidden = false;
+		}
+		
+	})	
+	
+	document.querySelectorAll('.hide-plate').forEach(button => {
+		const parentPlate = button.parentElement.parentElement
+		button.onclick = () => {
+			parentPlate.querySelector('.plate-body').hidden = true;
+			parentPlate.querySelector('.show-plate').hidden = false;
+			parentPlate.querySelector('.hide-plate').hidden = true;
+		}
+		
+	})	
+	
+	document.querySelectorAll('.show-used-crystals').forEach(image => {
+		const parentPlate = image.parentElement.parentElement
+		image.onclick = () => {
+			parentPlate.querySelector('.used').style.display = "flex";
+			parentPlate.querySelector('.show-used-crystals').hidden = true;
+			parentPlate.querySelector('.hide-used-crystals').hidden = false;
+		}
+		
+	})	
+	
+	document.querySelectorAll('.hide-used-crystals').forEach(image => {
+		const parentPlate = image.parentElement.parentElement
+		image.onclick = () => {
+			parentPlate.querySelector('.used').style.display = "none";
+			parentPlate.querySelector('.show-used-crystals').hidden = false;
+			parentPlate.querySelector('.hide-used-crystals').hidden = true;
+		}
+		
+	})	
 })
--- compound_screen/views.py
+++ compound_screen/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
+import string
 
 def generate_table_body(html_string):
 	rows = ""
 	for i in range(61):
 		rows = rows + "<tr><td>" + str(i+1) + "</td>" + html_string + "</tr>\n"
 	
-	return rows
+	return rows	
+
+def generate_well_names(highest_letter, highest_number): 
+	
+	#generate lists of strings to concatenate into well names
+	part_1 = list(string.ascii_uppercase)[0:highest_letter]
+	part_2 = []
+	for x in range(1,highest_number):
+		if x<10:
+			part_2.append('0' + str(x))
+		else:
+			part_2.append(str(x))
+
+	part_3 = [['a', 'c'], ['d']]
+
+	#generate an ordered list of all well names in $naming_system_1
+	well_name_list = []
+	
+	for number in part_2:
+		for charlist in part_3:
+			for cap in part_1:
+				for letter in charlist:
+					well_name_list.append(cap + number + letter)
+	return well_name_list
+	
+wells = generate_well_names(8, 13)
+wells_small = generate_well_names(3, 4)
+wells_small_1 = wells_small[0:8]
+wells_small_2 = wells_small[8:-1]
 
 def source(request):
 	return render(request, "compound_screen/source.html", {
@@ -20,11 +49,10 @@ def crystals(request):
 	"title": "Crystal gallery",
 	"header": "CRYSTAL GALLERY",
 	"table_caption": "Crystals", 
-	"counter1": range(1,15),
-	"counter2": range(1,20),
-	"counter3": range(1,8),
-	"counter4": range(1,30),
-	
+	#"wells": wells,
+	"wells": wells_small,
+	"wells_part_1": wells_small_1,
+	"wells_part_2": wells_small_2
 	})
 
 def batches(request):
