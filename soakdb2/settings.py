cb106c0272b18276f9fa754b1762071cb6dc200b
Marta Rudnicka <marta_rudnicka@vp.pl>
2020-10-30 22:08:18 +0000

    one directory for static files used by all apps

--- soakdb2/settings.py
+++ soakdb2/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "/home/arl45673/project/django/soakdb2/static",
+  
+]
---
+++ static/functions.js
@@ -0,0 +1,118 @@
+/*use [#hideButton] to hide all elements in elementList and show [#showButton] */
+function hideElementList(hideButton, elementList, showButton){
+		document.getElementById(hideButton).addEventListener("click", function(){
+		elementList.forEach(cell => {
+			cell.style.display = 'none';
+		})
+		document.getElementById(showButton).style.display = 'inline-block';
+		return false;
+	})
+}
+
+/*use [#showButton] to show all elements in elementList*/
+function showElementList(showButton, elementList) {
+	document.getElementById(showButton).addEventListener("click", function() {
+		elementList.forEach(cell => {
+			cell.style.display = 'table-cell';
+		})	
+		document.getElementById(showButton).style.display = 'none';
+		return false;
+	})
+}
+
+/*when [#button] is clicked, check all checkboxes in listOfCheckboxes */
+function checkAllByButton(button, listOfCheckboxes) {
+	document.getElementById(button).onclick = () => {
+		listOfCheckboxes.forEach( checkbox => {
+			if (checkbox.checked === false)
+				checkbox.click();
+		})
+	}
+}
+
+
+/*when [#button] is clicked, uncheck all checkboxes in listOfCheckboxes */
+function uncheckAllByButton(button, listOfCheckboxes) {
+	document.getElementById(button).onclick = () => {
+		listOfCheckboxes.forEach( checkbox => {
+			if (checkbox.checked == true) {
+				checkbox.click();
+			}
+		})
+	}
+}
+
+/*extract inner text from the select element into a string */
+function getSelectString(elementID) {
+	const option = document.getElementById(elementID).value;
+	var targetString = document.querySelector(`[value=${option}]`).text;
+	if (option === 'null') {
+		targetString = null;
+	}	
+	return targetString;
+}
+
+/*extract input value into a string */
+function getInputString(elementID) {
+	var targetString = String(document.getElementById(elementID).value);
+	if (targetString == '') {
+	targetString = null;
+	}
+	return targetString;	
+}
+
+/*hides an array of elements by their selectors*/
+function hide(array) {
+	for (i = 0; i < array.length; i++) {
+		document.querySelector(array[i]).style.display = "none";
+	} 
+}
+
+/*shows an array of lements setting their display attributes to displayType*/
+function show(array, displayType) { 
+	for (i = 0; i < array.length; i++) {
+		document.querySelector(array[i]).style.display = displayType;
+	} 
+}
+
+/*sets the values of all elements in ${array} to ${cvalue}*/
+function setAllElementValuesTo(array, cvalue) {
+	console.log('cvalue = ', cvalue);
+	for (i = 0; i < array.length; i++) {
+		document.querySelector(array[i]).value = cvalue;
+	} 
+}
+
+function showWhenChecked(checkboxID, elementID, displayType){
+	const element = document.getElementById(elementID);
+	const checkbox = document.getElementById(checkboxID);
+	if (checkbox.checked === true) {
+		element.style.display = displayType;
+	}
+	else {
+		element.style.display = 'none';
+	}
+}
+
+function countChecked(checkboxIdList, displayElementId) {
+	var counter = 0;
+	checkboxIdList.forEach(id => {	
+	const checkbox = document.getElementById(id);
+	if (checkbox.checked === true) {
+			counter++;
+		}
+	})
+	document.getElementById(displayElementId).innerHTML = counter;
+}
+
+function showHiddenClass(className){
+	document.querySelectorAll(className).forEach(element => {
+		element.style.visibility = 'visible';
+	})
+}
+
+function hideClass(className){
+	document.querySelectorAll(className).forEach(element => {
+		element.style.visibility = 'hidden';
+	})
+}
